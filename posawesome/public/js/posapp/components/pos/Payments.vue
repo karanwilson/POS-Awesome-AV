e0305209d3788ce606172a2b297dfc672ce040ab
Karan Wilson <karan.wilson@auroville.org.in>
2025-05-23 08:42:14 +0530

    CI

--- posawesome/public/js/posapp/components/pos/Payments.vue
+++ posawesome/public/js/posapp/components/pos/Payments.vue
@@ -1109,7 +1109,7 @@ export default {
         data["remarks"] = this.order_remarks_text;
 
         //console.log("this.pos_profile.posa_allow_credit_sale: ", this.pos_profile.posa_allow_credit_sale);
-        if (totalPayedAmount === 0 && this.redeemed_customer_credit === 0 && this.is_credit_sale === 0) {
+        if (totalPayedAmount == 0 && this.redeemed_customer_credit == 0 && this.is_credit_sale == 0) {
           evntBus.$emit("show_mesage", {
             text: "Please set a Mode of Payment",
             color: "error",
