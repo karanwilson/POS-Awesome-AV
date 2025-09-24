fcb1c9120eb0c80c2b67d720c90fc3827ead47dd
Karan Wilson <karan.wilson@auroville.org.in>
2025-09-24 09:08:34 +0530

    CI

--- posawesome/public/js/posapp/components/pos/Invoice.vue
+++ posawesome/public/js/posapp/components/pos/Invoice.vue
@@ -1511,8 +1511,8 @@ export default {
         this.update_discount_umount();
       let sum = 0;
       this.items.forEach((item) => {
-        //sum += flt(item.qty) * flt(item.rate);
-        sum += flt(item.amount);
+        sum += flt(item.qty, this.currency_precision) * flt(item.rate, this.rounding_method);
+        //sum += flt(item.amount);
       });
       sum -= this.flt(this.discount_amount);
       sum += this.flt(this.delivery_charges_rate);
